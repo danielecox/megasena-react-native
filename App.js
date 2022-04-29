7545e841a558840c2d1117b6dc057cb3b537952f
Daniele Marighetti Cox <40305033+danielecox@users.noreply.github.com>
2022-04-28 22:58:31 -0300

    Add files via upload
    upload project

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  let [numeros, setNumeros] = useState([]);
+
+  const gerarMegaSena = () => {
+    numeros = []; // limpa o array anterior
+    while (numeros.length != 6) {
+      // verifica se tem os 6 n. caso nao continua o codigo
+      numeros.push(Math.floor(Math.random() * 60) + 1); // gera n entre 1 e 60
+
+      numeros = numeros.filter((item, index) => {
+        // verifica se tem repeticao nos numeros gerados
+        return numeros.indexOf(item) === index;
+      });
+    }
+
+    setNumeros([numeros]); // seta o array com os 6 n gerados
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cardView}>
+        <Text style={styles.tituloText}>Sortear números:</Text>
+        <Button title="Gerar números da MEGA SENA" onPress={gerarMegaSena} />
+        <Text style={styles.subTituloText}>
+          {" "}
+          {numeros.map((numero) => `${numero}`)}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tituloText: {
+    marginBottom: 16,
+    fontSize: 22,
+    color: "#696969",
+  },
+  subTituloText: {
+    marginTop: 16,
+    fontSize: 20,
+    borderRadius: 10,
+    color: "#b32E37",
+  },
+  cardView: {
+    textAlign: "center",
+    borderWidth: 3,
+    borderColor: "#6c2a6a",
+    padding: 15,
+    backgroundColor: "#ffebee",
+    borderRadius: 10,
+    boxShadow: "inset 0 0 1em silver, 0 0 1em #6c2a6a",
+  }
+});
